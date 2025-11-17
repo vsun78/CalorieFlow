745f6faec3bec62abecace2e4628b55d179377b7
vsun78 <vsun7@uwo.ca>
2025-11-16 22:30:04 -0500

    Added calculate calories method, get calories from node list method, and an output method that displays calories remaining, and fixed some bugs regarding multiple outputs

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const selectList = document.getElementById("selectList");
 const entryButton = document.getElementById("entry-button");
 const calculateButton = document.getElementById("calculate-button");
 const clearButton = document.getElementById("clear-button");
+const outputSection = document.getElementById("output");
 
 //functions
 
@@ -21,7 +22,65 @@ function addEntry(stringValue)
     <input type="number" min ="0" id="calInput" required/>
     </div>
     `;
+}
 
-    
+calculateButton.addEventListener("click",calculateCalories);
+function calculateCalories(e){
+    e.preventDefault(); // prevent page refresh as its unneeded
+
+    // get all the input calories for calculations
+    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+    const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
+    const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
+    const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
+    const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+
+    const breakfastTotalCalories = getCalories(breakfastNumberInputs);
+    const lunchTotalCalories = getCalories(lunchNumberInputs);
+    const dinnerTotalCalories = getCalories(dinnerNumberInputs);
+    const snacksTotalCalories = getCalories(snacksNumberInputs);
+    const exerciseTotalCalories = getCalories(exerciseNumberInputs);
+
+    // remaining calories calculation
+    const remainingCalories = Number(budget.value) + exerciseTotalCalories - breakfastTotalCalories - 
+    lunchTotalCalories - dinnerTotalCalories - snacksTotalCalories;
+
+    output(remainingCalories);
 
 }
+
+// get total calories
+function getCalories(listOfInputs)
+{
+    let result = 0;
+
+    for(const inputElement of listOfInputs)
+    {
+        result += Number(inputElement.value); // cause value is string
+    }
+
+    return result;
+}
+
+
+// output function for the output at the bottom of the page
+function output(calRemaining)
+{   
+    outputSection.innerHTML = ``; // reset so output doesnt stack
+
+    if(calRemaining >= 0)
+    {
+        outputSection.innerHTML += `<fieldset id="fs-remaining">
+        <h2>${calRemaining} calories remaining </h2>
+        </fieldset>`;
+    }
+    else{
+        outputSection.innerHTML += `<fieldset id="fs-excess">
+        <h2>${calRemaining} calories in excess</h2>
+        </fieldset>`;
+    }
+    
+}
+
+// clear output as well
+clearButton.addEventListener("click", () => outputSection.innerHTML = ``);
