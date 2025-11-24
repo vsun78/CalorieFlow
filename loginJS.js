9415c89ceb5cb1a35f7311c10d24659306604a4f
vsun78 <vsun7@uwo.ca>
2025-11-23 19:22:12 -0500

    Added a login js file and changed up some id's in signup.html

---
+++ loginJS.js
@@ -0,0 +1,8 @@
+// DOM variables
+const email = document.getElementById("emailSignupInput");
+const username = document.getElementById("usernameSignupInput");
+const password = document.getElementById("passwordSignupInput");
+const loginButton = document.getElementById("signup-button");
+
+// functions
+
